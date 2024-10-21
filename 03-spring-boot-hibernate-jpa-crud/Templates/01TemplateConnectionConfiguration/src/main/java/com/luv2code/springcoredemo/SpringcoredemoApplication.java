c0fbe9dee4713dfe010cf5c3f9dd24bf76d6a16a
DanielGS1572 <gonzalisd@gmail.com>
2024-10-20 22:43:55 -0600

    spring

---
+++ 03-spring-boot-hibernate-jpa-crud/Templates/01TemplateConnectionConfiguration/src/main/java/com/luv2code/springcoredemo/SpringcoredemoApplication.java
@@ -0,0 +1,6 @@
+package com.luv2code.springcoredemo;
+
+
+public class SpringcoredemoApplication {
+
+}
---
+++ 03-spring-boot-hibernate-jpa-crud/Templates/02TemplateDAOConnection/src/main/java/com/luv2code/springcoredemo/SpringcoredemoApplication.java
@@ -0,0 +1,18 @@
+package com.luv2code.springcoredemo;
+
+/*
+Crear la interfaz del DAO y su implementación
+Crear su entity con los siguientes atributos
+- id        - con su generation type
+- firstName - nombre de columna first_name
+- lastName  - nombre de columna last_name
+- email
+
+Generar getters/setter e implementar el toString()
+
+Cuando se guarde un student desplegar su id en consola
+
+*/
+
+public class SpringcoredemoApplication {
+}
