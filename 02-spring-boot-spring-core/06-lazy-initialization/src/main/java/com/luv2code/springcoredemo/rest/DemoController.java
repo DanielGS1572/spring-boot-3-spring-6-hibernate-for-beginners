0c4b81c5dd5c6f357ce1a6de6f7ade5fd5503b84
DanielGS1572 <gonzalisd@gmail.com>
2024-12-08 20:00:02 -0600

    spring katas

--- 02-spring-boot-spring-core/06-lazy-initialization/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
+++ 02-spring-boot-spring-core/06-lazy-initialization/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -3,6 +3,7 @@ package com.luv2code.springcoredemo.rest;
 import com.luv2code.springcoredemo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +14,7 @@ public class DemoController {
     private Coach myCoach;
 
     @Autowired
-    public DemoController(@Qualifier(/*"trackCoach"*/ "tennisCoach") Coach theCoach) {
+    public DemoController(@Qualifier("trackCoach" /*"tennisCoach"*/) Coach theCoach) {
         System.out.println("In constructor: " + getClass().getSimpleName());
         myCoach = theCoach;
     }
--- 04-spring-boot-rest-crud/05-spring-boot-rest-crud-students-exception-handling/src/main/java/com/luv2code/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/05-spring-boot-rest-crud-students-exception-handling/src/main/java/com/luv2code/demo/rest/StudentRestController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
-@RestController
+@RestController     //El controller es el rest service
 @RequestMapping("/api")
 public class StudentRestController {
 
